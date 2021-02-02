dcbc6f82a6211d5ef0434a1f02c54174df514343
davidyang2149 <davidyang2149@gmail.com>
2021-02-02 12:27:53 +0900

    Update App - CSS Reset, @emotion/css Add
    MyBakingRecipe
    - Css Reset(emotion check).
    - @emotion/css Add.

--- src/App.tsx
+++ src/App.tsx
@@ -1,74 +1,68 @@
 import React from 'react';
 
+import { injectGlobal } from '@emotion/css'
+
 import styled from '@emotion/styled';
 
-const Main = styled.main({
-  /* Desktop */
-  position: 'relative',
-  width: 1440,
-  height: 900,
-  background: '#FFFFFF',
-})
+injectGlobal`
+/* 
+  Origin: http://meyerweb.com/eric/tools/css/reset/ 
+  v2.0 | 20110126
+  License: none (public domain)
+*/
 
-const Header = styled.header({
-  position: 'absolute',
-  width: 1440,
-  height: 70,
-  left: 0,
-  top: 0,
-});
-
-const HeaderLine = styled.div({
-  position: 'absolute',
-  width: 1440,
-  height: 0,
-  left: 0,
-  top: 70,
-
-  border: '1px solid #DBDBDB',
-})
-
-const Title = styled.p({
-  position: 'absolute',
-  width: 300,
-  height: 20,
-  left: 30,
-  // top: 15,
-
-  fontFamily: 'Karla Tamil Inclined',
-  fontStyle: 'italic',
-  fontWeight: 'normal',
-  fontSize: 20,
-  // lineHeight: 23,
-
-  color: '#000000',
-});
-
-const SearchBox = styled.input({
-  /* search box */
-position: 'absolute',
-width: 300,
-height: 36,
-left: 570,
-top: 17,
-
-background: '#FAFAFA',
-border: '1px solid #DBDBDB',
-boxSizing: 'border-box',
-borderRadius: 2,
-});
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed, 
+figure, figcaption, footer, header, hgroup, 
+menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+	margin: 0;
+	padding: 0;
+	border: 0;
+	font-size: 100%;
+	font: inherit;
+	vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure, 
+footer, header, hgroup, menu, nav, section {
+	display: block;
+}
+body {
+	line-height: 1;
+}
+ol, ul {
+	list-style: none;
+}
+blockquote, q {
+	quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+	content: '';
+	content: none;
+}
+table {
+	border-collapse: collapse;
+	border-spacing: 0;
+}
+`
 
 export default function App() {
   return (
-    <Main>
-      <Header>
-        <Title>My Baking Recipe</Title>
-        <SearchBox />
-        <HeaderLine />
-      </Header>
+    <>
+      <p>My Baking Recipe</p>
       <div>
         레시피명
       </div>
-    </Main>
+    </>
   );
 }

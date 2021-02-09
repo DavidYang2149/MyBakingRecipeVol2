1c1971eddfeb2a1ce1eb9af66554568b82e2f209
davidyang2149 <davidyang2149@gmail.com>
2021-02-09 21:45:23 +0900

    Update RecipePage.test.tsx
    MyBakingRecipe
    - Update TestCode(100%).

--- src/pages/__test__/RecipePage.test.tsx
+++ src/pages/__test__/RecipePage.test.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
 import { render } from '@testing-library/react';
 
+import { RootState } from '../../redux/rootReducer';
+import allConditionsState from '../../../fixtures/allConditionsState';
 import RecipePage from '../RecipePage';
 
+jest.mock('react-redux');
+
 describe('RecipePage', () => {
+  const dispatch = jest.fn();
+
+  (useDispatch as jest.Mock).mockImplementation(() => dispatch);
+  (useSelector as jest.Mock)
+    .mockImplementation((selector: (arg: RootState) => void) => selector(allConditionsState));
+
   it('render Page', () => {
-    const { container } = render(<RecipePage />);
+    const { container } = render((
+      <MemoryRouter>
+        <RecipePage />
+      </MemoryRouter>
+    ));
 
     expect(container).toHaveTextContent('레시피 작성 페이지');
   });

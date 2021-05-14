e19de7f851bf4555d686c61c6c28aa83a13e3a57
davidyang2149 <davidyang2149@gmail.com>
2021-05-14 21:08:41 +0900

    Update App
    MyBakingRecipe Vol2
    - Update App.

--- src/App.tsx
+++ src/App.tsx
@@ -10,9 +10,6 @@ import TodosPage from './pages/todo/TodosPage';
 export default function App() {
   return (
     <>
-      <h1 className="sample">Hello Sample Todo</h1>
-      <h1 className="sample customFont">This is Custom Font</h1>
-      <p className="nyancat">With NyanCat</p>
       <Switch>
         <Route exact path="/" component={TodosPage} />
         <Route exact path="/Hello" component={HelloPage} />

a486c17ac28e3035c080c86933308d9b6b468c70
davidyang2149 <davidyang2149@gmail.com>
2021-05-17 21:30:27 +0900

    Update App.tsx
    MyBakingRecipeVol2
    - Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,12 @@ import NotFoundPage from './pages/common/NotFoundPage';
 import HelloPage from './pages/common/HelloPage';
 import TodosPage from './pages/todo/TodosPage';
 
+import HeaderContainer from './containers/common/HeaderContainer';
+
 export default function App() {
   return (
     <>
+      <HeaderContainer />
       <Switch>
         <Route exact path="/" component={TodosPage} />
         <Route exact path="/Hello" component={HelloPage} />

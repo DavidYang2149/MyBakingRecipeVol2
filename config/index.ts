a5bea58d70b63e8ce98cef01b449afa40bfc7bf7
davidyang2149 <davidyang2149@gmail.com>
2021-05-18 22:07:57 +0900

    Add config (option)
    MyBakingRecipeVol2
    - Add config (option).

---
+++ config/index.ts
@@ -0,0 +1,8 @@
+import devConfig from 'config/dev';
+import prodConfig from 'config/prod';
+
+const config = process.env.NODE_ENV === 'production'
+  ? prodConfig
+  : devConfig;
+
+export default config;

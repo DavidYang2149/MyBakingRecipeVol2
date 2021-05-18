09bb00060fd6bdea6c5324b44c59d831bfc575a8
davidyang2149 <davidyang2149@gmail.com>
2021-05-18 22:07:19 +0900

    Add config
    MyBakingRecipeVol2
    - Add config.
      - for env.

---
+++ config/type/config.ts
@@ -0,0 +1,11 @@
+export type Config = {
+  apiKey: string | undefined;
+  authDomain: string | undefined;
+  databaseURL: string | undefined;
+  projectId: string | undefined;
+  storageBucket: string | undefined;
+  messagingSenderId: string | undefined;
+  appId: string | undefined;
+  measurementId: string | undefined;
+  status: string;
+};

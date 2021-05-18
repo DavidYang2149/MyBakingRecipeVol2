a8466ef7be1ccdac01fbd382a24319e30a01e2c2
davidyang2149 <davidyang2149@gmail.com>
2021-05-18 21:28:17 +0900

    Update rootReducer redux
    MyBakingRecipeVol2
    - Update rootReducer redux.
      - Add user.

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -2,10 +2,12 @@ import { combineReducers } from '@reduxjs/toolkit';
 
 import todoReducer from 'src/redux/todo/todo';
 import todosReducer from 'src/redux/todo/todos';
+import userReducer from 'src/redux/user/user';
 
 const rootReducer = combineReducers({
   todo: todoReducer,
   todos: todosReducer,
+  user: userReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;

39f3669cc352a92adbfa8331fa5513128029538f
davidyang2149 <davidyang2149@gmail.com>
2021-05-31 22:41:56 +0900

    Add Loading components With TestCode, SCSS
    MyBakingRecipeVol2
    - Add Loading components With TestCode, SCSS.

---
+++ src/components/__test__/common/Loading.test.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Loading from 'src/components/common/Loading';
+
+describe('Loading', () => {
+  it('render Loading', () => {
+    render(<Loading />);
+  });
+});

b7e070170dc547c2de824cc5ff18c0c8e120d912
davidyang2149 <davidyang2149@gmail.com>
2021-07-22 22:04:28 +0900

    :white_check_mark: Refactor TestCode RecipeItem (renaming, split variable)
    MyBakingRecipeVol2
    - 변수를 쪼개여 의도를 명확하게 전달하기.
    - 이름 다시 짓기.

--- src/components/__test__/recipe/RecipeItem.test.tsx
+++ src/components/__test__/recipe/RecipeItem.test.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 
-import RecipeItem, { RecipeItemProps } from 'src/components/recipe/RecipeItem';
+import RecipeItem, { Props } from 'src/components/recipe/RecipeItem';
 
 describe('RecipeItem', () => {
-  const renderBlockItem = ({ recipe }: RecipeItemProps) => render((
+  const renderBlockItem = ({ recipe }: Props) => render((
     <MemoryRouter>
       <RecipeItem
         recipe={recipe}
@@ -13,9 +13,9 @@ describe('RecipeItem', () => {
     </MemoryRouter>
   ));
 
-  context('with description less then 30 words', () => {
+  context('with recipe', () => {
     it('render RecipeItem', () => {
-      const mockRecipe = {
+      const theMockRecipe = {
         id: 'a1',
         image: '',
         title: '초코칩쿠키',
@@ -27,20 +27,25 @@ describe('RecipeItem', () => {
         ingredients: [],
         upload: '',
       };
-      const { container } = renderBlockItem({ recipe: mockRecipe });
+      const { container } = renderBlockItem({ recipe: theMockRecipe });
 
       expect(container).toHaveTextContent('초코칩쿠키');
+      expect(container).toHaveTextContent('test');
+      expect(container).not.toHaveTextContent('test@email.com');
+
       expect(container).toHaveTextContent('초코칩 쿠키 만들기');
     });
   });
 
-  context('with description more then 30 words', () => {
-    it('render RecipeItem', () => {
-      const mockRecipe = {
+  context('with recipe', () => {
+    it('render RecipeItem cut down description', () => {
+      const the30Letters = '초코칩 쿠키 만들기를 제작해봅시다. 첫 번째 초코칩 쿠';
+      const theOver30Letters = `${the30Letters}키를 잘게 다진다. 두 번째 설탕을 넣는다.`;
+      const theMockRecipe = {
         id: 'a1',
         image: '',
         title: '초코칩쿠키',
-        description: '초코칩 쿠키 만들기를 제작해봅시다. 첫 번째 초코칩 쿠키를 잘게 다진다. 두 번째 설탕을 넣는다.',
+        description: theOver30Letters,
         created: '2021-07-02-18:10:12',
         userId: 'test@email.com',
         product: 10,
@@ -48,12 +53,13 @@ describe('RecipeItem', () => {
         ingredients: [],
         upload: '',
       };
-      const { container } = renderBlockItem({ recipe: mockRecipe });
+      const { container } = renderBlockItem({ recipe: theMockRecipe });
 
       expect(container).toHaveTextContent('초코칩쿠키');
-      expect(container).toHaveTextContent('초코칩 쿠키 만들기');
       expect(container).toHaveTextContent('test');
       expect(container).not.toHaveTextContent('test@email.com');
+
+      expect(container).toHaveTextContent('초코칩 쿠키 만들기를 제작해봅시다. 첫 번째 초코칩 쿠...');
     });
   });
 });

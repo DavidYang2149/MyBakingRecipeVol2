935fbe6d74f8c31432f6cf239f9d2d49aeb0e03e
davidyang2149 <davidyang2149@gmail.com>
2021-07-27 21:47:47 +0900

    :hammer: #4 Update NotFound (theBaseImageURL로 변경)
    MyBakingRecipeVol2
    - img src 변경.

--- src/components/common/NotFound.tsx
+++ src/components/common/NotFound.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import { theBaseImageURL } from 'src/utils/static';
+
 import './NotFound.scss';
 
 const NotFound = () => {
   return (
     <div className="theNotFoundModal">
-      <img className="theNotFoundImage" id="displayImage" src="assets/images/page-not-found.svg" alt="recipe" />
+      <img
+        className="theNotFoundImage"
+        src={`${theBaseImageURL}assets/images/page-not-found.svg`}
+        alt="recipe"
+      />
       <p className="theNotFoundTitle">
         해당 페이지를 찾을 수 없습니다.
       </p>

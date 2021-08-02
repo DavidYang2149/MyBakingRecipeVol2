fd7dd9d58ff81bfdd93ad8913ad0d423ef7b1637
davidyang2149 <davidyang2149@gmail.com>
2021-08-02 22:15:04 +0900

    :hammer: #6 Update tools utils (formatRecipe, changeDateToString 추가)
    MyBakingRecipeVol2
    - 함수 chanageDateToString 추가
    - 함수 formatRecipe 추가
    - static RECIPE_COUNT 추가(TODO: 리팩토링할 것)

--- src/utils/tools.ts
+++ src/utils/tools.ts
@@ -1,3 +1,12 @@
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+
+import firebase from 'firebase/app';
+
+import { Recipe } from 'src/types/recipe';
+import { ResponseRecipe, Timestamp } from 'src/types/response';
+
+export const RECIPE_COUNT = 12;
+
 export const isEmpty = (value: unknown) => {
   if (!value) {
     return true;
@@ -22,4 +31,30 @@ export const isNotMatch = (left: unknown) => (right: unknown) => {
 
 export const splitReturnFirst = (value: string) => (keyword: string) => {
   return value.split(keyword)[0];
+};
+
+export const changeDateToString = (date: Timestamp) => {
+  return date.toDate().toISOString();
+};
+
+export const formatRecipe = (recipe: firebase.firestore.QueryDocumentSnapshot<ResponseRecipe>): Recipe => {
+  const { created, updated } = recipe.data();
+  const {
+    userId, title, product, category, newIngredient, ingredients, description, upload, image, show,
+  } = recipe.data();
+  return {
+    id: recipe.id,
+    userId,
+    title,
+    product,
+    category,
+    newIngredient,
+    ingredients,
+    description,
+    upload,
+    image,
+    show,
+    created: changeDateToString(created!),
+    updated: changeDateToString(updated!),
+  };
 };

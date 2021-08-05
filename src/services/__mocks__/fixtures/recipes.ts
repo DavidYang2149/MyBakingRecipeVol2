939df3e808798541cce5355fa72db403f507173c
davidyang2149 <davidyang2149@gmail.com>
2021-08-05 21:03:26 +0900

    :clown_face: #8 Update Mock recipes fixtures (Add ResponseRecipe)
    MyBakingRecipeVol2
    - Mock recipes fixtures 추가 (ResponseRecipe[])
    - Redux recipes 테스트코드 작성용

--- src/services/__mocks__/fixtures/recipes.ts
+++ src/services/__mocks__/fixtures/recipes.ts
@@ -1,4 +1,9 @@
+import { fireStore } from 'src/services/firebase/firebase';
+
 import { RecipesState } from 'src/types/recipe';
+import { ResponseRecipe } from 'src/types/response';
+
+const date = fireStore.Timestamp.now();
 
 const recipes: RecipesState = {
   recipesBook: [
@@ -25,5 +30,28 @@ const recipes: RecipesState = {
   ],
   lastRecipe: { recipe: null, isLast: false },
 };
+
+export const responseRecipes: ResponseRecipe[] = [
+  {
+    id: '1',
+    userId: '1',
+    title: '마들렌',
+    category: 1,
+    product: 16,
+    ingredients: [
+      { id: 1, ingredient: '설탕', weight: 150 },
+      { id: 2, ingredient: '버터', weight: 150 },
+      { id: 3, ingredient: '전란', weight: 100 },
+      { id: 4, ingredient: '박력분', weight: 150 },
+    ],
+    newIngredient: { id: 0, ingredient: '', weight: 0 },
+    description: '마들렌 만드는 방법. 오븐 180도에 10분간 굽기',
+    created: date,
+    updated: date,
+    show: true,
+    upload: '',
+    image: '',
+  },
+];
 
 export default recipes;

8effa6e7e4c0c182de2557f2764005dc493bf2aa
davidyang2149 <davidyang2149@gmail.com>
2021-08-10 22:29:14 +0900

    :white_check_mark: #12 Update TestCode RecipesPage (isIntersecting 테스트 추가 / 고정 이슈)
    MyBakingRecipeVol2
    - RcipesPage 테스트 코드 수정
    - isIntersecting 핸들링 추가(Coverage 100%)
    - Lint 이슈 발생 (FIXED ME)

--- src/pages/__test__/recipe/RecipesPage.test.tsx
+++ src/pages/__test__/recipe/RecipesPage.test.tsx
@@ -1,4 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unnecessary-type-assertion */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable prefer-destructuring */
 /* eslint-disable func-names */
+
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
@@ -40,5 +47,51 @@ describe('ReipcesPage', () => {
     expect(container).toHaveTextContent('마들렌');
   });
 
-  // TODO: UPDATE TEST COVERAGE
+  context('with isIntersecting', () => {
+    it('render Page', () => {
+      const { container } = render((
+        <MemoryRouter>
+          <ReipcesPage />
+        </MemoryRouter>
+      ));
+
+      // FIXED ME: Lint 문제 해결하기
+      const calls = window.IntersectionObserver.mock.calls;
+      const [onChange] = calls[calls.length - 1];
+
+      onChange([
+        {
+          target: container.firstChild,
+          isIntersecting: true,
+          intersectionRatio: 0.5,
+        },
+      ]);
+
+      expect(dispatch).toBeCalled();
+    });
+  });
+
+  context('without isIntersecting', () => {
+    it('render Page', () => {
+      const { container } = render((
+        <MemoryRouter>
+          <ReipcesPage />
+        </MemoryRouter>
+      ));
+
+      // FIXED ME: Lint 문제 해결하기
+      const calls = window.IntersectionObserver.mock.calls;
+      const [onChange] = calls[calls.length - 1];
+
+      onChange([
+        {
+          target: container.firstChild,
+          isIntersecting: false,
+          intersectionRatio: 0.5,
+        },
+      ]);
+
+      expect(dispatch).not.toBeCalled();
+    });
+  });
 });

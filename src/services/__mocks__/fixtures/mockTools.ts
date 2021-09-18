ce6f42d888544debce7f7cfebfcb3cbf8589daf2
davidyang2149 <davidyang2149@gmail.com>
2021-09-18 22:16:16 +0900

    :clown_face: #105 useDispatch, useSelector Mock의 반복을 제거하라
    MyBakingRecipeVol2
    - useDispatch, useSelector 공통 Mock 추가

--- src/services/__mocks__/fixtures/mockTools.ts
+++ src/services/__mocks__/fixtures/mockTools.ts
@@ -1,3 +1,5 @@
+import { useDispatch, useSelector } from 'react-redux';
+
 import user from 'src/services/__mocks__/fixtures/user';
 import recipes from 'src/services/__mocks__/fixtures/recipes';
 
@@ -6,5 +8,9 @@ const mockState = {
   recipe: recipes.recipesBook[0],
   recipes,
 };
+
+export const mockUseDispatch = useDispatch as jest.Mock;
+
+export const mockUseSelector = useSelector as jest.Mock;
 
 export default mockState;

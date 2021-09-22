1601fff21c4cb9b86af08a6227c0f9e1527ad6f6
davidyang2149 <davidyang2149@gmail.com>
2021-09-22 13:33:40 +0900

    :lipstick: #78 공통 컴포넌트 SCSS 클래스명을 수정하라
    MyBakingRecipeVol2
    - common component SCSS 클래스명 수정

--- src/components/common/NotFound.tsx
+++ src/components/common/NotFound.tsx
@@ -6,13 +6,13 @@ import './NotFound.scss';
 
 const NotFound = () => {
   return (
-    <div className="theNotFoundModal">
+    <div className="div-not-found-modal">
       <img
-        className="theNotFoundImage"
+        className="img-not-found-image"
         src={`${theBaseImageURL}assets/images/page-not-found.svg`}
         alt="recipe"
       />
-      <p className="theNotFoundTitle">
+      <p className="p-not-found-title">
         해당 페이지를 찾을 수 없습니다.
       </p>
     </div>
